15ff6607dd6169369ecadc15129bbf4c9613d3a2
ctasantanajr <ctasantanajr@gmail.com>
2020-02-27 12:49:24 +0000

    first commit/second class

---
+++ server.js
@@ -0,0 +1,29 @@
+var logger = require("morgan"),
+cors = require("cors"),
+http = require("http"),
+express = require("express"),
+bodyParser = require("body-parser");
+
+var app = express();
+var port = 3000;
+
+app.use(bodyParser.json());
+
+app.get('/hello', function(req,res){
+    res.json({result: 'Hello CCT!'});
+});
+
+app.get('/hello/:foo/:bar', function(req,res){
+    res.json({result: 'Hello CCT', data: [
+        req.params.foo,
+        req.params.bar
+    ]});
+});
+
+app.post('/hello', function(req,res){
+    res.json({result: 'The POST request was sent!', data: req.body});
+});
+
+app.listen(port,function(err){
+    console.log("Listening on port:" + port);
+});
